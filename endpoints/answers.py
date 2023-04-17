0e381a0e01586de02c4b731a6937c0e18529bd80
Igor Tolmachov <me@igorek.dev>
2023-04-17 16:23:00 +0900

    Add websockets

--- endpoints/answers.py
+++ endpoints/answers.py
@@ -1,13 +1,35 @@
-from fastapi import APIRouter
+from asyncio import Queue
+from uuid import UUID
+
+from fastapi import APIRouter, HTTPException, WebSocket
 from fastapi.exceptions import RequestValidationError
 from pydantic import ValidationError
 from sqlalchemy import and_, delete, or_, select
+from websockets.exceptions import ConnectionClosed
 
 import database
+import models as m
 from endpoints import dependencies
 from models import answers as models
 
 router = APIRouter(prefix="/answers", tags=["Answers"])
+values_pull: dict[int, set[Queue[models.Value]]] = {}
+questions_pull: dict[int, set[Queue[m.poll.Question]]] = {}
+
+
+def put_values(poll_id: int, values: list[models.Value]) -> None:
+    if poll_id not in values_pull:
+        return
+    for queue in (q for q in values_pull[poll_id]):
+        for value in values:
+            queue.put_nowait(value)
+
+
+def put_questions(poll_id: int, question: m.poll.Question) -> None:
+    if poll_id not in questions_pull:
+        return
+    for queue in (q for q in questions_pull[poll_id]):
+        queue.put_nowait(question)
 
 
 @router.post("/add/value")
@@ -53,9 +75,12 @@ async def add_value(
         await session.refresh(answer)
 
         try:
-            return models.Answer.from_orm(answer)
+            answer_model = models.Answer.from_orm(answer)
         except ValidationError as e:
             raise RequestValidationError(e.raw_errors)
+
+        put_values(poll_id, answer_model.answer.values)
+        return answer_model
 
 
 @router.post("/add/answer")
@@ -75,7 +100,39 @@ async def add_answer(
         await session.flush()
         await session.refresh(answer)
 
-        return models.Answer.from_orm(answer)
+        try:
+            answer_model = models.Answer.from_orm(answer)
+        except ValidationError as e:
+            raise RequestValidationError(e.raw_errors)
+
+        put_values(poll_id, answer_model.answer.values)
+        return answer_model
+
+
+@router.post("/send/question")
+async def send_questions(
+    user: dependencies.User,
+    poll_id: int,
+    question_id: UUID,
+) -> None:
+    async with database.sessions.begin() as session:
+        poll = await session.scalar(
+            select(database.Poll).where(
+                and_(
+                    database.Poll.id == poll_id,
+                    database.Poll.owner_id == user.id,
+                )
+            )
+        )
+
+        if poll is None:
+            raise HTTPException(400, "Poll not found")
+
+        uuids = m.poll.Poll.from_orm(poll).poll.uuids
+        if question_id not in uuids:
+            raise HTTPException(400, "Question not found")
+
+        put_questions(poll_id, uuids[question_id])
 
 
 @router.get("/get/id")
@@ -161,11 +218,41 @@ async def delete_answers(user: dependencies.User, poll_id: int) -> None:
         )
 
 
-# @router.websocket("/listen/question")
-# async def listen_question() -> None:
-#     pass
+@router.websocket("/listen/values")
+async def listen_values(websocket: WebSocket, poll_id: int) -> None:
+    await websocket.accept()
+    if poll_id not in values_pull:
+        values_pull[poll_id] = set()
+
+    queue: Queue[models.Value] = Queue()
+    values_pull[poll_id].add(queue)
+
+    try:
+        while True:
+            message = await queue.get()
+            await websocket.send_text(message.json())
+    except ConnectionClosed:
+        values_pull[poll_id].remove(queue)
+        if len(values_pull[poll_id]) == 0:
+            values_pull.pop(poll_id)
+        await websocket.close()
 
 
-# @router.websocket("/listen/values")
-# async def listen_values() -> None:
-#     pass
+@router.websocket("/listen/questions")
+async def listen_questions(websocket: WebSocket, poll_id: int) -> None:
+    await websocket.accept()
+    if poll_id not in questions_pull:
+        questions_pull[poll_id] = set()
+
+    queue: Queue[m.poll.Question] = Queue()
+    questions_pull[poll_id].add(queue)
+
+    try:
+        while True:
+            message = await queue.get()
+            await websocket.send_text(message.json())
+    except ConnectionClosed:
+        questions_pull[poll_id].remove(queue)
+        if len(questions_pull[poll_id]) == 0:
+            questions_pull.pop(poll_id)
+        await websocket.close()
